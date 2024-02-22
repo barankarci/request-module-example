7c60f4a33ea00ff7796df096571d7706d7230c22
Baran Karcı <barankarcim@gmail.com>
2024-02-23 01:40:01 +0300

    async öğrendim kayıt

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const request = require("request");
-request(`https://jsonplaceholder.typicode.com/posts/1`, (error, response, body) => {
-    // console.log(error);
-    console.log(typeof body);
-    const json = JSON.parse(body);
-    console.log(typeof json)
-    console.log(json.userId);
-    
-})
+
+const getData = (url) => {
+    return new Promise((resolve, reject) => {
+        (async () => {
+            request(url, (error, response, body) => {
+                const jsonData = JSON.parse(body);
+                resolve(jsonData)
+
+            })
+        })()
+    })
+}
+
+getData(`https://jsonplaceholder.typicode.com/posts`).then(result => {
+    console.log({result:result})
+    getData(`https://jsonplaceholder.typicode.com/posts/1`).then(result2 => {
+        console.log({result2:result2})
+    });
+
+
+});
